6b2943e352ee7d51d699d516cb8f0377112624aa
youwol-guillaume <greinisch@youwol.com>
2023-04-30 08:55:27 +0200

    ✨ Presentation.examples => add 'Hello world' example

--- src/app/presentation/examples.ts
+++ src/app/presentation/examples.ts
@@ -1,5 +1,34 @@
 export const examples = [
     {
+        title: 'Hello world',
+        description: {
+            innerHTML:
+                'Basics of importing resources using <a href="https://getbootstrap.com/docs/4.0/components/dropdowns/">bootstrap example</a>. ' +
+                'Installation goes over indirect dependencies installation first and then link bootstrap appropriately.',
+        },
+        src: `return async (cdnClient, message$) => {
+    await cdnClient.install({
+    // bootstrap will come with its dependencies installed first (e.g. popper.js)
+        modules:['bootstrap#^4.0.0'],
+    // CSS are explicitly imported 
+        css: ['bootstrap#^4.4.0~bootstrap.min.css']
+    })
+    const div = document.createElement('div')
+    div.innerHTML =  \`
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Dropdown button
+    </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item">Action</a>
+    <a class="dropdown-item">Another action</a>
+    <a class="dropdown-item">Something else here</a>
+  </div>
+</div>\`
+    return div
+}`,
+    },
+    {
         title: 'Real world',
         description: {
             innerHTML:
