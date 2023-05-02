275c3a3eeb793a36b3ed99a6c6164b95c8ffa409
youwol-guillaume <greinisch@youwol.com>
2023-05-02 15:27:32 +0200

    ♻️ browse packages => use NPM to fetch info

--- src/app/browse/browse.view.ts
+++ src/app/browse/browse.view.ts
@@ -1,45 +1,25 @@
-import { VirtualDOM, children$ } from '@youwol/flux-view'
-export {}
+import { VirtualDOM, children$, child$ } from '@youwol/flux-view'
+import { BehaviorSubject } from 'rxjs'
+import { NpmPackageResponse, NpmSearchResponse } from './npm.models'
 import { AssetsGateway } from '@youwol/http-clients'
-import { forkJoin, Observable } from 'rxjs'
-import { map, tap } from 'rxjs/operators'
-import { raiseHTTPErrors } from '@youwol/http-primitives'
+import { onHTTPErrors } from '@youwol/http-primitives'
+
 class State {
-    public readonly items$: Observable<{ name: string }[]>
-    public readonly folders = [
-        '0d4cbe1e-7792-4d9b-a2f1-cc4242d098a6',
-        '2b89091f-eb62-441b-9253-1870e6fe35e0',
-        '9916e0db-3b07-4699-a2c8-bbc9727727f7',
-        '6a0b0c24-d8bf-463e-b694-a69b3751e0bf',
-        '445005a2-c69c-416a-becb-6bf5f7a04010',
-        'f4aa0761-d008-4ac3-9406-58fc3c7bb188',
-    ]
-    constructor() {
-        const client = new AssetsGateway.Client()
-        this.items$ = forkJoin(
-            this.folders.map((parentId) => {
-                return client.explorer
-                    .queryChildren$({
-                        parentId,
-                    })
-                    .pipe(raiseHTTPErrors())
-            }),
-        ).pipe(
-            map((results) => {
-                return results.map((resp) => resp.items).flat()
-            }),
-            tap((d) => console.log(d)),
-            map((d) =>
-                [...new Set(d.map((l) => l.name))]
-                    .sort()
-                    .map((name) => ({ name })),
-            ),
-        )
+    public readonly packages$ = new BehaviorSubject<NpmSearchResponse>({
+        objects: [],
+    })
+
+    search(term: string) {
+        fetch(`https://registry.npmjs.com/-/v1/search?text=${term}&size=20`)
+            .then((resp) => resp.json())
+            .then((d) => {
+                this.packages$.next(d)
+            })
     }
 }
 export class BrowseView implements VirtualDOM {
     public readonly state = new State()
-    public readonly class = 'w-100 flex-grow-1 overflow-auto'
+    public readonly class = 'w-100 flex-grow-1 d-flex flex-column'
     public readonly style = {
         minHeight: '0px',
         fontFamily: 'Lexend, sans-serif',
@@ -48,16 +28,19 @@ export class BrowseView implements VirtualDOM {
 
     constructor() {
         this.children = [
-            new SearchBar(),
+            new SearchBar({ state: this.state }),
             new ResultsView({ state: this.state }),
         ]
     }
 }
 
 export class SearchBar implements VirtualDOM {
+    public readonly state: State
     public readonly class = 'my-2 w-100'
     public readonly children = []
-    constructor() {
+    constructor(params: { state: State }) {
+        Object.assign(this, params)
+
         this.children = [
             {
                 class: 'w-50 d-flex align-items-center mx-auto',
@@ -88,6 +71,9 @@ export class SearchBar implements VirtualDOM {
                                 tag: 'input',
                                 type: 'text',
                                 placeholder: 'Search packages',
+                                onchange: (ev) => {
+                                    this.state.search(ev.target.value)
+                                },
                             },
                         ],
                     },
@@ -98,18 +84,130 @@ export class SearchBar implements VirtualDOM {
 }
 
 export class ResultsView implements VirtualDOM {
-    public readonly class = 'mx-auto'
-    public readonly style = {
-        maxWidth: '800px',
-    }
+    public readonly class = 'w-100 flex-grow-1 overflow-auto px-2'
+
     public readonly state: State
-    public readonly children
+    public readonly children: VirtualDOM
+
     constructor(params: { state: State }) {
         Object.assign(this, params)
-        this.children = children$(this.state.items$, (items) => {
-            return items.map((item) => ({
-                innerText: item.name,
-            }))
-        })
+        this.children = [
+            {
+                class: 'mx-auto',
+                style: {
+                    maxWidth: '800px',
+                },
+                children: children$(this.state.packages$, (response) => {
+                    return response.objects.map(
+                        (item) => new PackageView(item.package),
+                    )
+                }),
+            },
+        ]
+    }
+}
+
+export class PackageView implements VirtualDOM {
+    public readonly class = 'fv-border-bottom-disabled py-4'
+    public readonly children: VirtualDOM[]
+
+    constructor(params: NpmPackageResponse) {
+        const client = new AssetsGateway.Client()
+        this.children = [
+            {
+                class: 'd-flex align-items-center',
+                children: [
+                    {
+                        style: { fontSize: '1.25rem', fontWeight: 'bolder' },
+                        innerText: params.name,
+                    },
+                    { class: 'mx-2' },
+                    {
+                        style: { fontSize: '1.25rem', weight: 'bolder' },
+                        innerText: params.version,
+                    },
+                    { class: 'flex-grow-1' },
+                ],
+            },
+            {
+                innerText: params.description,
+            },
+            child$(
+                client.cdn
+                    .getLibraryInfo$({
+                        libraryId: window.btoa(params.name),
+                    })
+                    .pipe(onHTTPErrors((_error) => undefined)),
+                (info) => {
+                    if (info == undefined) {
+                        return new PackageNotAvailableView()
+                    }
+                    if (info.versions[0] != params.version) {
+                        console.log(info.versions[0], params.version)
+                        return new UpgradeAvailableView({
+                            latest: info.versions[0],
+                        })
+                    }
+                    return new VersionAvailableView()
+                },
+                { untilFirst: { class: 'fas fa-spinner fa-spin' } },
+            ),
+        ]
+    }
+}
+
+class VersionAvailableView {
+    public readonly class =
+        'border p-2 d-flex align-items-center fv-text-success  fv-pointer'
+    public readonly style = {
+        width: 'fit-content',
+    }
+    public readonly children: VirtualDOM[]
+    constructor() {
+        this.children = [
+            {
+                class: 'fas fa-check',
+            },
+            { class: 'mx-2' },
+            {
+                innerText: 'Version available',
+            },
+        ]
+    }
+}
+class UpgradeAvailableView {
+    public readonly class = 'border p-2 d-flex align-items-center  fv-pointer'
+    public readonly style = {
+        width: 'fit-content',
+    }
+    public readonly children: VirtualDOM[]
+    constructor(params: { latest }) {
+        this.children = [
+            {
+                class: 'fas fa-cloud-upload-alt',
+            },
+            { class: 'mx-2' },
+            {
+                innerText: `Request publication (latest: ${params.latest})`,
+            },
+        ]
+    }
+}
+class PackageNotAvailableView {
+    public readonly class = 'border p-2 d-flex align-items-center fv-pointer'
+    public readonly style = {
+        width: 'fit-content',
+    }
+    public readonly children: VirtualDOM[]
+    constructor() {
+        this.children = [
+            {
+                class: 'fas fa-cloud-upload-alt',
+            },
+            { class: 'mx-2' },
+            {
+                innerText: 'Request publication',
+            },
+        ]
     }
 }
