2cc6544fc1e48b47658c4a5071df5be09140d3bc
youwol-guillaume <greinisch@youwol.com>
2023-11-12 00:01:24 +0100

    ♻️ [Presentation] => clean & improve CDN section

--- src/app/presentation/cdn-section.view.ts
+++ src/app/presentation/cdn-section.view.ts
@@ -1,80 +1,11 @@
 import { VirtualDOM } from '@youwol/flux-view'
 import { setup } from '../../auto-generated'
 
-const tableData = {
-    rowHeaders: ['NPM', 'jsDelivr', 'WebPM'],
-    columnHeaders: [
-        'Target',
-        'Dynamic install',
-        'Dependencies resolution',
-        'aliases',
-        'multi versions support',
-        'Package publication',
-    ],
-    values: [
-        ['PC', 'Browsers', 'Browsers'],
-        [false, true, true],
-        [true, false, false],
-        [true, false, false],
-        [true, false, false],
-        ['easy', 'easy', 'medium'],
-    ],
-}
-const checked = `<i class="fas fa-check fv-text-success">`
-const notChecked = `<i class="fas fa-times fv-text-error">`
-const table = `
- <table>
-        <thead>
-            <tr>
-                <th></th>
-                <th>Host</th>
-                <th>Dynamic install</th>
-                <th>Dependencies resolution</th>
-                <th>Aliases</th>
-                <th>Versions mixin</th>
-                <th>Package publication</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><strong>WebPM</strong></td>
-                <td class="w-100 d-flex justify-content-center"><img width=32 src="/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/browsers.png"></td>
-                <td>${checked}</td>
-                <td>${checked}</td>
-                <td>${checked}</td>
-                <td>${checked}</td>
-                <td>medium</td>
-            </tr>
-            <tr>
-                <td>jsDelivr</td>
-                <td class="w-100 d-flex justify-content-center"><img class='mx-center' width=32 src="/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/browsers.png"></td>
-                <td>${checked}</td>
-                <td>${notChecked}</td>
-                <td>${notChecked}</td>
-                <td>${notChecked}</td>
-                <td>easy</td>
-            </tr>
-            <tr>
-                <td>NPM</td>
-                <td class="w-100 d-flex justify-content-center"><img class='mx-center' width=48 src="/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/PC.jpg"></td>
-                <td>${notChecked}</td>
-                <td>${checked}</td>
-                <td>${checked}</td>
-                <td>${checked}</td>
-                <td>easy</td>
-            </tr>
-        </tbody>
-    </table>
-`
 export class CdnSectionView {
-    public readonly class = 'mx-auto'
+    public readonly class = 'mx-auto border-left border-bottom p-5'
     public readonly children: VirtualDOM[]
     constructor() {
         this.children = [
-            {
-                class: 'separator',
-            },
-            { class: 'my-5' },
             {
                 innerText: 'Flexible and robust',
                 style: {
@@ -84,8 +15,6 @@ export class CdnSectionView {
             },
             { class: 'my-4' },
             new GridView(),
-            { class: 'my-5' },
-            new TableComparisonView(),
         ]
     }
 }
@@ -93,7 +22,7 @@ export class CdnSectionView {
 class Cell implements VirtualDOM {
     public readonly class = 'col-md-4 d-flex  align-items-center'
     public readonly children: VirtualDOM[]
-    constructor({ imageName, text }) {
+    constructor({ imageName, text, title }) {
         this.children = [
             {
                 tag: 'img',
@@ -102,35 +31,26 @@ class Cell implements VirtualDOM {
                 src: `/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/${imageName}`,
             },
             {
-                class: 'm-2',
-                tag: 'h6',
-                style: {
-                    fontWeight: '600',
-                },
-                innerText: text,
+                class: 'd-flex flex-column m-2',
+                children: [
+                    {
+                        tag: 'h6',
+                        style: {
+                            fontWeight: '600',
+                        },
+                        innerText: title,
+                    },
+                    { class: 'text-justify', innerText: text },
+                ],
             },
         ]
     }
 }
-
-class TableComparisonView implements VirtualDOM {
-    public readonly class = ''
+class EmptyCell implements VirtualDOM {
+    public readonly class = 'col-md-4 d-flex  align-items-center'
     public readonly children: VirtualDOM[]
     constructor() {
-        this.children = [
-            {
-                innerText: 'Package managers comparison',
-                style: {
-                    fontSize: '1.2rem',
-                    fontWeight: 'bolder',
-                },
-            },
-            { class: 'my-4' },
-            {
-                class: 'text',
-                innerHTML: table,
-            },
-        ]
+        this.children = []
     }
 }
 class GridView implements VirtualDOM {
@@ -141,35 +61,51 @@ class GridView implements VirtualDOM {
             {
                 class: 'row',
                 children: [
-                    new Cell({
-                        imageName: 'browsers.png',
-                        text: 'Install in any browser',
-                    }),
+                    new EmptyCell(),
                     new Cell({
                         imageName: 'flexible.jpg',
-                        text: 'Flexible install at any time ',
+                        title: 'On the fly install',
+                        text: 'Runtimes can be extended dynamically, creating a range of new opportunities.',
                     }),
+                    new EmptyCell(),
+                ],
+            },
+            {
+                class: 'row',
+                children: [
+                    new Cell({
+                        imageName: 'browsers.png',
+                        title: 'Install in any browser',
+                        text: 'Share your code and runtime, ready to execute from a simple URL.',
+                    }),
+                    new EmptyCell(),
                     new Cell({
                         imageName: 'dependencies.png',
-                        text: 'Dependencies graph resolution',
+                        title: 'Dependencies graph resolution',
+                        text: 'Properly handle direct and indirect dependencies along with dynamic linking.',
                     }),
                 ],
             },
             {
                 class: 'row mt-5',
                 children: [
-                    new Cell({
-                        imageName: 'safe.png',
-                        text: 'Imports using aliases',
-                    }),
+                    new EmptyCell(),
                     new Cell({
                         imageName: 'multi-versions.png',
-                        text: 'Mixin versions support',
+                        title: 'Mixin versions support',
+                        text: 'Installed runtimes can feature library with multiple versions.',
                     }),
-                    new Cell({
-                        imageName: 'packaging.png',
-                        text: 'Port your project or NPM packages easily',
-                    }),
+                    new EmptyCell(),
+                    // new Cell({
+                    //     imageName: 'safe.png',
+                    //     title: 'Imports using aliases',
+                    //     text: 'Conveniently assign aliases to your imports.',
+                    // }),
+                    // new Cell({
+                    //     imageName: 'packaging.png',
+                    //     title: 'Port your project or NPM packages easily',
+                    //     text: '',
+                    // }),
                 ],
             },
         ]
