ce2e597eff8146a27f502020c61119721fbb0d3a
youwol-guillaume <greinisch@youwol.com>
2023-11-13 14:51:48 +0100

    ♻️ `AppView` => use `rx-vdom`

--- src/app/app.view.ts
+++ src/app/app.view.ts
@@ -1,4 +1,5 @@
-import { child$, VirtualDOM } from '@youwol/flux-view'
+import { VirtualDOM, ChildrenLike, FluxViewVirtualDOM } from '@youwol/rx-vdom'
+
 import { TopBannerView } from './top-banner.view'
 import { PresentationView } from './presentation/presentation.view'
 import { BehaviorSubject } from 'rxjs'
@@ -6,26 +7,27 @@ import { AboutView } from './presentation/about.view'
 
 export type Topic = 'Home' | 'About'
 
-export class AppView implements VirtualDOM {
+export class AppView implements VirtualDOM<'div'> {
+    public readonly tag = 'div'
     public readonly topic$ = new BehaviorSubject<Topic>('Home')
     public readonly class = 'h-100  w-100 d-flex flex-column'
 
-    public readonly children: VirtualDOM[]
+    public readonly children: ChildrenLike
 
     constructor() {
         this.children = [
-            new TopBannerView({ topic$: this.topic$ }),
-            child$(this.topic$, (topic) => {
-                if (topic == 'Home') {
-                    return new PresentationView()
-                }
-                if (topic == 'About') {
-                    return new AboutView()
-                }
-                // if (topic == 'Playground') {
-                //     return new JsPlaygroundView()
-                // }
-            }),
+            new TopBannerView({ topic$: this.topic$ }) as FluxViewVirtualDOM,
+            {
+                source$: this.topic$,
+                vdomMap: (topic: Topic) => {
+                    if (topic == 'Home') {
+                        return new PresentationView() as FluxViewVirtualDOM
+                    }
+                    if (topic == 'About') {
+                        return new AboutView() as FluxViewVirtualDOM
+                    }
+                },
+            },
         ]
     }
 }
