0bef01dc991f59563f177a28ef256e097a12dd2f
youwol-guillaume <greinisch@youwol.com>
2023-11-16 07:16:37 +0100

    🔧 add `rx-vdom-config.ts`

--- template.py
+++ template.py
@@ -1,13 +1,21 @@
 import shutil
 from pathlib import Path
 
-from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
-    RunTimeDeps, generate_template, DevServer, Bundles, MainModule
+from youwol.pipelines.pipeline_typescript_weback_npm import (
+    Template,
+    PackageType,
+    Dependencies,
+    RunTimeDeps,
+    generate_template,
+    DevServer,
+    Bundles,
+    MainModule,
+)
 from youwol.utils import parse_json
 
 folder_path = Path(__file__).parent
 
-pkg_json = parse_json(folder_path / 'package.json')
+pkg_json = parse_json(folder_path / "package.json")
 
 externals = {
     "@youwol/webpm-client": "^2.2.0",
@@ -16,44 +24,44 @@ externals = {
     "@youwol/rx-vdom": "^1.0.0",
     "rxjs": "^6.5.5",
     "@youwol/http-clients": "^2.0.5",
-    "@youwol/http-primitives": "^0.1.2"
+    "@youwol/http-primitives": "^0.1.2",
 }
 
 template = Template(
     path=folder_path,
     type=PackageType.Application,
-    name=pkg_json['name'],
-    version=pkg_json['version'],
-    shortDescription=pkg_json['description'],
-    author=pkg_json['author'],
+    name=pkg_json["name"],
+    version=pkg_json["version"],
+    shortDescription=pkg_json["description"],
+    author=pkg_json["author"],
     dependencies=Dependencies(
-        runTime=RunTimeDeps(
-            externals=externals
-        ),
+        runTime=RunTimeDeps(externals=externals),
         devTime={
             "lz-string": "^1.4.4"
         }
     ),
     bundles=Bundles(
-      mainModule=MainModule(
-          entryFile='./main.ts',
-          loadDependencies=list(externals.keys())
-      )
+        mainModule=MainModule(
+            entryFile="./main.ts", loadDependencies=list(externals.keys())
+        )
     ),
     userGuide=True,
-    devServer=DevServer(
-        port=3017
-    )
+    devServer=DevServer(port=3017),
 )
 
 generate_template(template)
 shutil.copyfile(
-    src=folder_path / '.template' / 'src' / 'auto-generated.ts',
-    dst=folder_path / 'src' / 'auto-generated.ts'
+    src=folder_path / ".template" / "src" / "auto-generated.ts",
+    dst=folder_path / "src" / "auto-generated.ts",
 )
-for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'webpack.config.ts']:
-    shutil.copyfile(
-        src=folder_path / '.template' / file,
-        dst=folder_path / file
-    )
+for file in [
+    "README.md",
+    ".gitignore",
+    ".npmignore",
+    ".prettierignore",
+    "LICENSE",
+    "package.json",
+    # "tsconfig.json", need to reference rx-vdom-config.ts
+    "webpack.config.ts",
+]:
+    shutil.copyfile(src=folder_path / ".template" / file, dst=folder_path / file)
