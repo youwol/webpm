a6c8d85bebf10b3778ff0c346ab5c5f5741693e8
youwol-guillaume <greinisch@youwol.com>
2023-11-27 13:25:51 +0100

    ♻️ [about] => redesign using modals for info

--- src/app/about/browser-as-os.view.ts
+++ src/app/about/browser-as-os.view.ts
@@ -1,6 +1,6 @@
 import { ChildrenLike, VirtualDOM } from '@youwol/rx-vdom'
 import { paragraphStyle, SectionTitle } from './common'
-import { setup } from '../../auto-generated'
+import { Cell } from './cell.view'
 
 export class BrowserAsOSSectionView implements VirtualDOM<'div'> {
     public readonly tag: 'div'
@@ -13,19 +13,6 @@ export class BrowserAsOSSectionView implements VirtualDOM<'div'> {
                 subtitle: 'Turn browsers into OS',
             }),
             { tag: 'div', class: 'my-4' },
-            {
-                tag: 'div',
-                class: 'd-flex justify-content-center',
-                children: [
-                    {
-                        tag: 'img',
-                        style: {
-                            height: '250px',
-                        },
-                        src: `/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/web-os.png`,
-                    },
-                ],
-            },
             { tag: 'div', class: 'my-4' },
             {
                 tag: 'div',
@@ -36,15 +23,132 @@ export class BrowserAsOSSectionView implements VirtualDOM<'div'> {
                         tag: 'p',
                         innerHTML: `WebPM establishes the groundwork for an emulated OS within browsers.
                         Because applications encompass more than just installing runtimes (user management, persistent
-                         storage, indexed databases, installers, environment, and more), YouWol offers for developers
-                          a set of solutions (backend APIs & frontend libraries) to fully take advantage of this OS 
-                          like environment. <br> <br>  You can visit for instance our 
-                          <a href='https://platform.youwol.com/applications/@youwol/platform/latest' target="_blank">desktop</a> 
-                          application to get a better idea of what we are talking of.
-                          More information on that subject will come soon.`,
+                         storage, indexed databases, installers, environment, and more), YouWol offers for users and 
+                         developers  a set of solutions to fully take advantage of this OS like environment.`,
                     },
                 ],
             },
+            new GridView(),
+
+            {
+                tag: 'div',
+                class: 'w-75 mx-auto',
+                style: paragraphStyle,
+                children: [
+                    {
+                        tag: 'p',
+                        innerHTML: `For a comprehensive understanding and detailed insights into the broader spectrum 
+                        of the topic, encompassing areas such as HTTP clients, assisting libraries, testing configurations, 
+                        and more, we encourage readers to explore the <a href="">dedicated page</a>.`,
+                    },
+                ],
+            },
+        ]
+    }
+}
+
+class GridView implements VirtualDOM<'div'> {
+    public readonly tag = 'div'
+    public readonly class = ' d-flex justify-content-center flex-wrap mt-4'
+    public readonly children: ChildrenLike
+    constructor() {
+        this.children = [
+            new Cell({
+                imageName: 'os-applications.png',
+                title: `Essentials Apps`,
+                text: `
+
+Our innovative solution redefines the traditional computing paradigm by transforming a web browser into a 
+fully functional operating system. This approach brings forth a collection of purpose-built applications, 
+each designed to deliver essential functionalities within a familiar and intuitive user experience.
+
+### Essential Applications for a Seamless User Experience:
+
+#### **Desktop Application:**
+   Our desktop application within the browser serves as the central hub for user interaction. 
+   Mimicking the traditional desktop environment, it provides a familiar space for managing applications, 
+   files, shortcuts, dashboard widgets, fostering a seamless transition for users accustomed to conventional 
+   operating systems.
+
+#### **Files Explorer Application:**
+   The files explorer application enhances user productivity by offering an intuitive interface for organizing,
+    accessing, and managing files within the browser-based operating system.
+     Users can navigate folders, preview documents, and perform file-related tasks seamlessly.
+
+#### **Application Hub:**
+   Our solution includes an application hub that acts as a repository for a diverse range of web-based applications.
+    Users can easily discover and launch applications directly from their browser, creating a unified ecosystem for 
+    diverse computing needs.
+
+#### **Usual UX Paradigm:**
+   Emphasizing a usual User Experience (UX) paradigm, our solution ensures that users encounter a computing 
+   environment that aligns with their expectations. The transition to a web browser as an operating system is smooth, 
+   thanks to the incorporation of familiar application interfaces and navigation patterns.
+
+#### **Collaboration and Integration:**
+   Beyond individual applications, our solution fosters collaboration and integration. 
+   Users can seamlessly switch between applications, share files, and collaborate in real-time, creating a cohesive and
+    interconnected computing experience.
+<!--
+## 2. **Advantages of Browser-Based OS:**
+   - **Platform Agnostic:** Accessible across different devices and operating systems.
+   - **Easy Updates:** Applications and features are updated seamlessly without requiring manual intervention.
+   - **Collaborative Workspace:** Facilitates collaborative work with shared documents and applications.
+-->`,
+            }),
+            new Cell({
+                imageName: 'os-backends.png',
+                title: 'Backend APIs',
+                text: `
+Backend APIs play a pivotal role in empowering developers to construct new apps seamlessly.
+ By providing a comprehensive set of APIs for essential backend functionalities, developers gain the foundation
+  needed to create robust and feature-rich applications. 
+#### **Storage Backend & Indexed Databases API:**
+Through the concept of asset (or custom domain project), developers can effortlessly  manage user-uploaded files as
+ well as  the creation of indexed databases, optimizing data access for improved 
+     performance in applications that involve large datasets.
+#### **Session Management API:**
+   The Session Management API facilitates secure and reliable session handling. Developers can seamlessly integrate 
+   session management capabilities into their apps, ensuring user authentication, authorization, and personalized 
+   experiences across sessions.
+#### **Data Management by Session API:**
+   Going a step further, the Data Management by Session API enables developers to associate data directly with user 
+   sessions. This allows for personalized data storage, retrieval, and modification based on user-specific contexts,
+    enhancing the customization potential of applications.
+#### **webPM Discovery API:**
+   The webPM Discovery API streamlines content delivery by providing developers with a dynamic approach to discover
+    and install libraries or applications. 
+`,
+            }),
+            new Cell({
+                imageName: 'os-projects.png',
+                title: 'Custom Domain Project',
+                text: `
+The point of a custom domain project (or custom asset) is to provide developers and users with the flexibility to define and
+ structure their projects according to their unique requirements, workflows, and objectives.
+
+#### **Tailored to the needs**
+ Custom domain projects allow developers to create projects that are specifically tailored to underlying requirements.
+ They can formalize a project using specific files organization as well as associated indexed document to 
+ access and manipulate data with speed and precision.
+
+#### **Enhanced User Experience**: 
+Custom domain projects can be bound to multiple applications and or libraries to enhance user experience.
+It can be for instance associating right click actions from the files-explorer, provide standard previews.
+Custom domain project usually comes with a concept of installer that gather multiple elements working together 
+to be included in the user's environment. 
+
+#### **Personalized Workflows**
+ The concept enables users to establish workflows that align with their preferences and the nature of the project.
+  This personalization can enhance efficiency and streamline the process of managing and collaborating on project-related tasks.
+ `,
+            }),
+            // new Cell({
+            //     imageName: 'os-tools.png',
+            //     title: 'Various helpers for developpers',
+            //     text: `The webOS ecosystem is also about a various set of HTTP-clients, helping libraries,
+            //     testing configurations, and more. `,
+            // }),
         ]
     }
 }
--- src/app/about/need.view.ts
+++ src/app/about/need.view.ts
@@ -1,6 +1,7 @@
 import { ChildrenLike, VirtualDOM } from '@youwol/rx-vdom'
 import { paragraphStyle, SectionTitle } from './common'
 import { setup } from '../../auto-generated'
+import { Cell } from './cell.view'
 
 export class NeedSectionView implements VirtualDOM<'div'> {
     public readonly tag: 'div'
@@ -13,11 +14,12 @@ export class NeedSectionView implements VirtualDOM<'div'> {
                 subtitle: 'Customization of PC, accessibility of servers',
             }),
             { tag: 'div', class: 'my-4' },
-            {
-                tag: 'div',
-                class: 'd-flex ',
-                children: [new PCView(), new ServersView()],
-            },
+            // {
+            //     tag: 'div',
+            //     class: 'd-flex ',
+            //     children: [new PCView(), new ServersView()],
+            // },
+            new GridView(),
             { tag: 'div', class: 'my-5' },
             {
                 class: 'w-75 mx-auto',
@@ -149,6 +151,73 @@ class PCView implements VirtualDOM<'div'> {
                 ],
             },
             new PCItemsView(),
+        ]
+    }
+}
+
+class GridView implements VirtualDOM<'div'> {
+    public readonly tag = 'div'
+    public readonly class = ' d-flex justify-content-center flex-wrap mt-4'
+    public readonly children: ChildrenLike
+    constructor() {
+        this.children = [
+            new Cell({
+                imageName: 'laptop.png',
+                title: `Customizable, not accessible`,
+                text: `
+Personal Computers (PCs) are renowned for their customizable nature, allowing users to tailor hardware and software
+ configurations to their specific needs. However, this customization, while empowering for the individual user,
+  presents challenges in terms of accessibility for others.
+
+### Challenges in Accessibility:
+
+#### **Dependency Management:**
+   Customized environments often involve a myriad of dependencies. Sharing a project or software developed in such an
+    environment can become challenging, as others may need to invest time in replicating the exact dependencies to 
+    ensure the application runs as intended.
+
+#### **Installation Complexity:**
+   The process of installing and configuring a customized environment can be complex, especially for individuals who
+    are not familiar with the specific tools and settings used by the developer.
+     This complexity poses a barrier to accessibility for those who wish to collaborate or use the software.
+
+#### **Versioning and Compatibility:**
+   Customized environments may rely on specific versions of libraries or tools. Ensuring version compatibility across
+    different machines becomes crucial, and the absence of standardized configurations can lead to versioning 
+    conflicts and difficulties in maintaining consistency.
+
+#### **Time and Learning Curve:**
+   Replicating a customized environment often requires a significant time investment and may involve a steep learning 
+   curve for those unfamiliar with the specific tools or development stacks used.
+    This can discourage collaboration and hinder the sharing of knowledge.
+
+`,
+            }),
+            new Cell({
+                imageName: 'servers.png',
+                title: 'Accessible, not customizable',
+                text: `
+Servers are designed to be accessible, allowing users to access and benefit from hosted services without the
+need for intricate installation processes. This accessibility is fundamental to the widespread availability of online platforms, applications, and data.
+
+### Challenges in Customization:
+
+#### **Limited User Control:**
+   While servers prioritize accessibility, the level of control granted to individual users is often limited. 
+   Backend services are typically managed by system administrators or hosting providers, restricting users' ability to 
+   customize underlying configurations or add new services.
+
+#### **Technical Expertise Requirements:**
+   Customizing or deploying backend services often demands a certain level of technical expertise.
+    Users without a background in server administration may find it challenging to implement specialized configurations 
+    or integrate custom functionalities.
+
+#### **Dependency on Service Providers:**
+   Users relying on servers are dependent on service providers for the customization and configuration of backend services. 
+   This dependency can be limiting for users who have specific requirements that fall outside the predefined offerings 
+   of the service provider.
+`,
+            }),
         ]
     }
 }
