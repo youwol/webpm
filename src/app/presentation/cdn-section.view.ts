cbc7d9832b91a0d14b08c56b71c04e674573f05d
youwol-guillaume <greinisch@youwol.com>
2023-11-28 11:20:15 +0100

    ♻️ [Presentation] => use factorized `CardView`

--- src/app/presentation/cdn-section.view.ts
+++ src/app/presentation/cdn-section.view.ts
@@ -1,6 +1,5 @@
 import { VirtualDOM, ChildrenLike } from '@youwol/rx-vdom'
-import { setup } from '../../auto-generated'
-
+import { CardView } from '../common/card.view'
 export class CdnSectionView implements VirtualDOM<'div'> {
     public readonly tag = 'div'
     public readonly class = 'mx-auto border-left border-bottom p-5'
@@ -21,92 +20,48 @@ export class CdnSectionView implements VirtualDOM<'div'> {
     }
 }
 
-class Cell implements VirtualDOM<'div'> {
-    public readonly tag = 'div'
-    public readonly class = 'col-md-4 d-flex  align-items-center'
-    public readonly children: ChildrenLike
-    constructor({ imageName, text, title }) {
-        this.children = [
-            {
-                tag: 'img',
-                width: 75,
-                height: 75,
-                src: `/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/${imageName}`,
-            },
-            {
-                tag: 'div',
-                class: 'd-flex flex-column m-2',
-                children: [
-                    {
-                        tag: 'h6',
-                        style: {
-                            fontWeight: 600,
-                        },
-                        innerText: title,
-                    },
-                    { tag: 'div', class: 'text-justify', innerText: text },
-                ],
-            },
-        ]
-    }
-}
-class EmptyCell implements VirtualDOM<'div'> {
-    public readonly tag = 'div'
-    public readonly class = 'col-md-4 d-flex  align-items-center'
-    public readonly children: ChildrenLike
-    constructor() {
-        this.children = []
-    }
-}
 class GridView implements VirtualDOM<'div'> {
     public readonly tag = 'div'
-    public readonly class = 'container mt-4'
+    public readonly class = 'd-flex flex-wrap justify-content-center'
     public readonly children: ChildrenLike
     constructor() {
         this.children = [
-            {
-                tag: 'div',
-                class: 'row',
-                children: [
-                    new EmptyCell(),
-                    new Cell({
-                        imageName: 'flexible.png',
-                        title: 'On the fly install',
-                        text: 'Runtimes can be extended dynamically, creating a range of new opportunities.',
-                    }),
-                    new EmptyCell(),
-                ],
-            },
-            {
-                tag: 'div',
-                class: 'row',
-                children: [
-                    new Cell({
-                        imageName: 'browsers.png',
-                        title: 'Install in any browser',
-                        text: 'Share your code and runtime, ready to execute from a simple URL.',
-                    }),
-                    new EmptyCell(),
-                    new Cell({
-                        imageName: 'dependencies.png',
-                        title: 'Dependencies graph resolution',
-                        text: 'Properly handle direct and indirect dependencies along with dynamic linking.',
-                    }),
-                ],
-            },
-            {
-                tag: 'div',
-                class: 'row',
-                children: [
-                    new EmptyCell(),
-                    new Cell({
-                        imageName: 'multi-versions.png',
-                        title: 'Mixin versions support',
-                        text: 'Installed runtimes can feature library with multiple versions.',
-                    }),
-                    new EmptyCell(),
-                ],
-            },
+            new CardView({
+                imageName: 'flexible.png',
+                title: 'On the fly install',
+                abstract: {
+                    tag: 'div',
+                    innerText:
+                        'Runtimes can be extended dynamically, creating a range of new opportunities.',
+                },
+            }),
+            new CardView({
+                imageName: 'browsers.png',
+                title: 'Any browser',
+                abstract: {
+                    tag: 'div',
+                    innerText:
+                        'Share your code and runtime, ready to execute from a simple URL.',
+                },
+            }),
+            new CardView({
+                imageName: 'dependencies.png',
+                title: 'Dynamic linking',
+                abstract: {
+                    tag: 'div',
+                    innerText:
+                        'Properly handle direct and indirect dependencies along with dynamic linking.',
+                },
+            }),
+            new CardView({
+                imageName: 'multi-versions.png',
+                title: 'Mixin versions',
+                abstract: {
+                    tag: 'div',
+                    innerText:
+                        'Installed runtimes can feature library with multiple versions.',
+                },
+            }),
         ]
     }
 }
--- src/app/presentation/developer-section.ts
+++ src/app/presentation/developer-section.ts
@@ -1,5 +1,6 @@
 import { VirtualDOM, ChildrenLike } from '@youwol/rx-vdom'
 import { setup } from '../../auto-generated'
+import { CardView, EmptyCard } from '../common/card.view'
 export class DeveloperSectionView implements VirtualDOM<'div'> {
     public readonly tag = 'div'
     public readonly class = 'mx-auto border-left p-5'
@@ -34,42 +35,6 @@ class TitleView implements VirtualDOM<'div'> {
     }
 }
 
-class CellHeader implements VirtualDOM<'div'> {
-    public readonly tag = 'div'
-    public readonly class = 'd-flex align-items-center'
-    public readonly children: ChildrenLike
-    constructor({ imageName, title }) {
-        this.children = [
-            {
-                tag: 'img',
-                width: 75,
-                height: 75,
-                src: `/api/assets-gateway/raw/package/${setup.assetId}/${setup.version}/assets/${imageName}`,
-            },
-            {
-                class: 'm-2',
-                tag: 'h6',
-                style: {
-                    fontWeight: 600,
-                },
-                innerText: title,
-            },
-        ]
-    }
-}
-class Cell implements VirtualDOM<'div'> {
-    public readonly tag = 'div'
-    public readonly class =
-        'col-md-4 d-flex  flex-column align-items-center text-justify'
-    public readonly children: ChildrenLike
-    constructor({ imageName, text, title }) {
-        this.children = [
-            new CellHeader({ imageName, title }),
-            { tag: 'div', class: 'm-2', innerHTML: text },
-        ]
-    }
-}
-
 class GridView implements VirtualDOM<'div'> {
     public readonly tag = 'div'
     public readonly class = 'container mt-4'
@@ -80,18 +45,24 @@ class GridView implements VirtualDOM<'div'> {
                 tag: 'div',
                 class: 'row',
                 children: [
-                    new Cell({
+                    new CardView({
                         imageName: 'TS.png',
                         title: 'Typescript friendly',
-                        text: `Dynamic does not mean "any": type checking, completion, <i>etc.</i> are 
+                        abstract: {
+                            tag: 'div',
+                            innerHTML: `Dynamic does not mean "any": type checking, completion, <i>etc.</i> are 
                             available transparently in your project. Check for instance <a href='https://github.com/youwol/todo-app-ts'>here </a>`,
+                        },
                     }),
-                    new EmptyCell(),
-                    new Cell({
+                    new EmptyCard(),
+                    new CardView({
                         imageName: 'laptop.png',
                         title: 'WebPM in your PC',
-                        text: `WebPM backend can run in your PC to facilitate working with your own projects.
+                        abstract: {
+                            tag: 'div',
+                            innerHTML: `WebPM backend can run in your PC to facilitate working with your own projects.
                                 Interested? Check a presentation <a href=''>here</a>.`,
+                        },
                     }),
                 ],
             },
@@ -99,24 +70,19 @@ class GridView implements VirtualDOM<'div'> {
                 tag: 'div',
                 class: 'row',
                 children: [
-                    new EmptyCell(),
-                    new Cell({
+                    new EmptyCard(),
+                    new CardView({
                         imageName: 'debug-browser.png',
                         title: 'In-browser debug',
-                        text: `We strive to provide the best experience to debug within the browser.
+                        abstract: {
+                            tag: 'div',
+                            innerText: `We strive to provide the best experience to debug within the browser.
                         Check for instance the 'youwol' libraries in the 'Sources' panel of your debug tool console.`,
+                        },
                     }),
-                    new EmptyCell(),
+                    new EmptyCard(),
                 ],
             },
         ]
-    }
-}
-class EmptyCell implements VirtualDOM<'div'> {
-    public readonly tag = 'div'
-    public readonly class = 'col-md-4 d-flex  align-items-center'
-    public readonly children: ChildrenLike
-    constructor() {
-        this.children = []
     }
 }
