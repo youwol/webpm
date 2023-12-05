d080ecfc74342ab5b261567eb71e35f3927ba57f
youwol-guillaume <greinisch@youwol.com>
2023-12-05 14:22:57 +0100

    🔥 remove useless export of `AboutYwModule`

--- src/app/on-load.ts
+++ src/app/on-load.ts
@@ -1,6 +1,4 @@
 import { render } from '@youwol/rx-vdom'
-import { aboutYouwolModule } from '@youwol/os-widgets'
-export const AboutYwModule = await aboutYouwolModule()
 import { AppView } from './app.view'
 
 export {}
